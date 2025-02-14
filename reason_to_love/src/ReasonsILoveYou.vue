72af57790cca2cb0a4780e38b9f92a09f5441405
Wesle dos Santos <wdsantoslds@gmail.com>
2025-02-14 16:47:27 -0700

    Feat: Mobile version

--- reason_to_love/src/ReasonsILoveYou.vue
+++ reason_to_love/src/ReasonsILoveYou.vue
@@ -5,6 +5,7 @@
     <div class="love-note">
       <div v-show="!startGameState && !finishGameState" class="welcome">
         <h2 class="title">For Whenever You Need a Reminder...</h2>
+        <h2 class="title">💌This list is still getting updated as I find more reasons to love you.💌</h2>
         <p class="subtitle">Click "Get a Reason" to feel the love. 💌</p>
       </div>
       <div v-show="isGameOver" class="game-over">
@@ -33,12 +34,22 @@ export default {
   data() {
     return {
       reasons: [
-        {index: 1, reason: "She always knows how to make me smile, no matter the situation."},
-        {index: 2, reason: "Her laugh is the most beautiful sound in the world to me."},
-        {index: 3, reason: "She supports me in my goals and encourages me to be my best self."},
-        {index: 4, reason: "Her kindness and compassion for others inspire me every day."},
-        {index: 5, reason: "She listens to me attentively and genuinely cares about what I have to say."},
-        {index: 6, reason: "Her hugs feel like home, comforting and safe."},
+        {index: 1, reason: "You are the most beautiful person I have ever met, inside and out."},
+        {index: 2, reason: "You makes me laugh. Specially when you make the 'GRINGA speaking spanish impersonation'."},
+        {index: 3, reason: "You are so beautiful, even when you don't try."},
+        {index: 4, reason: "You are so kind and sensitive to others. I love how you care about everyone around you. I still remember that they you stop that man in the institute hall to ask him if he was ok."},
+        {index: 5, reason: "You are so smart and talented. The first time I saw you play the piano, I was blown away."},
+        {index: 6, reason: "You are an amazing daughter and sister. The way you demonstrate your love for your family is so beautiful. It make me desire to be part of your family."},
+        {index: 7, reason: "You are an amazing friend. Not just for me, but for everyone."},
+        {index: 8, reason: "You were a faithful sister missionary. This makes me so proud of you. I still remember that day you showed me your scriptures and the notes you took in the mission. A sweet memory."},
+        {index: 9, reason: "You learn Portuguese just because of me. This is so sweet. I can see how much you love me in this gesture."},
+        {index: 10, reason: "You have a beautiful voice. I love to hear you sing. That Sunday when we were together with your mom at the Church, I was trying to keep up with your beautiful voice."},
+        {index: 11, reason: "You makes miracles in the kitchen. I love your food and I love when you cook for me. KEEP DOING IT."},
+        {index: 12, reason: "Your touch makes me feel so loved. Specially when you hold my hand."},
+        {index: 13, reason: "It's easy to love you and everyone likes you. It's like if you had an magnet to attract good people."},
+        {index: 13, reason: "Your smile is so cute! I feel like I'm in heaven when I see you smiling."},
+        {index: 14, reason: "Your eyes are so beautiful. I can feel your love when you look at me."},
+        {index: 15, reason: "I love that you want to serve your country. You make me proud."},
       ],
       startGameState: false,
       finishGameState: false,
@@ -70,8 +81,10 @@ export default {
 </script>
 
 <style scoped>
+
 .valentine-background {
   display: flex;
+  padding: 10px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -124,6 +137,7 @@ export default {
 
 .heart-button {
   background-color: #ff4f81;
+  margin-top: 20px;
   color: white;
   font-size: 18px;
   border: none;
@@ -144,7 +158,7 @@ export default {
   border: none;
   border-radius: 50px;
   padding: 12px 24px;
-  margin-top: 10px;
+  margin-top: 20px;
   cursor: pointer;
   transition: 0.3s;
 }
@@ -155,7 +169,45 @@ export default {
 .display-reason{
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+  row-gap: 20px;
+  height: 300px;
+}
+.welcome {
+  display: flex;
+  flex-direction: column;
   align-items: center;
   row-gap: 20px;
 }
+@media only screen and (max-width: 600px) {
+  .valentine-background {
+    display: flex;
+    padding: 20px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 90%;
+    background: linear-gradient(to bottom right, #ff758c, #ff7eb3);
+    font-family: "Cursive", Arial, sans-serif;
+    text-align: center;
+  }
+  .display-reason{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    width: 100%;
+    align-items: center;
+    row-gap: 10px;
+  }
+  .reason-text {
+    font-size: 1rem;
+    color: #d6336c;
+    font-weight: bold;
+    margin: 5px 0;
+  }
+
+}
+
 </style>
